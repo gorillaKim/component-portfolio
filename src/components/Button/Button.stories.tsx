53ef57db2ff9793b6642be4d848668f13f0538ef
gorillaKim <yhkim@madup.com>
2023-01-01 22:43:20 +0900

    feat: 컬러 및 사이즈 구분

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Button, {ButtonType} from "./Button"
+import Button, {ButtonType, ButtonSize} from "./Button"
 
 export default {
   component: Button,
@@ -13,4 +13,18 @@ export const Primary = () => (
 )
 export const Secondary = () => (
   <Button theme={ButtonType.SECONDARY}>secondary 버튼</Button>
+)
+
+export const Size = () => (
+  <>
+    <Button theme={ButtonType.PRIMARY} size={ButtonSize.SMALL}>
+      SMALL 버튼
+    </Button>
+    <Button theme={ButtonType.PRIMARY} size={ButtonSize.DEFAULT}>
+      DEFAULT 버튼
+    </Button>
+    <Button theme={ButtonType.PRIMARY} size={ButtonSize.LARGE}>
+      LARGE 버튼
+    </Button>
+  </>
 )
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,14 +8,25 @@ export enum ButtonType {
   SECONDARY = "secondary"
 }
 
+export enum ButtonSize {
+  DEFAULT = "default",
+  SMALL = "sm",
+  LARGE = "lg"
+}
+
 export interface IProps {
   children: React.ReactNode
   onClick?: React.MouseEventHandler
   theme?: ButtonType
+  size?: ButtonSize
 }
 
-const Button: React.FC<IProps> = ({children, theme = ButtonType.DEFAULT}) => {
-  const className = classNames(styles.default, styles[theme])
+const Button: React.FC<IProps> = ({
+  children,
+  theme = ButtonType.DEFAULT,
+  size = ButtonSize.DEFAULT
+}) => {
+  const className = classNames(styles.default, styles[theme], styles[size])
   return <button className={className}>{children}</button>
 }
 
