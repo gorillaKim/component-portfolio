6744a16600da2b34f18e675649f3a1c67e0d6f9b
gorillaKim <yhkim@madup.com>
2023-01-01 16:38:37 +0900

    feat: 모달 개발 완료

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,53 @@
-import React, {FC} from "react"
+import React, {CSSProperties, FC, ReactNode} from "react"
 import styles from "./Modal.module.sass"
 import classNames from "classnames"
+import Close from "../Icons/Close"
 
-export interface ModalProps {}
+export interface ModalProps {
+  visible?: boolean
+  title?: ReactNode
+  extraTitle?: ReactNode
+  footer?: ReactNode
+  onClose?: () => void
+  style?: CSSProperties
+  footerStyle?: CSSProperties
+  className?: string
+  children?: ReactNode
+}
 
-const Modal: FC<ModalProps> = () => (
-  <div className={classNames(styles.ModalBackDrop, {[styles.active]: true})}>
-    <div className={styles.Modal}>test</div>
-  </div>
-)
+const Modal: FC<ModalProps> = ({
+  visible,
+  title,
+  extraTitle,
+  footer,
+  onClose,
+  style,
+  footerStyle,
+  className,
+  children
+}) => {
+  const handleOnClose = (): void => onClose?.()
+
+  return (
+    <>
+      <div
+        className={classNames(styles.ModalBackDrop, {[styles.active]: visible})}
+        onClick={handleOnClose}
+      />
+      <div className={classNames(styles.Modal, {[styles.active]: visible})}>
+        <div className={classNames(styles.ModalBody, className)} style={style}>
+          <div className={styles.Title}>
+            <span>{title}</span>
+            <div>{extraTitle || <Close onClick={handleOnClose} />}</div>
+          </div>
+          <div>{children}</div>
+        </div>
+        <div className={styles.Footer} style={footerStyle}>
+          {footer}
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default Modal
